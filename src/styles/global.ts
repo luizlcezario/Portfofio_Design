9c03c7293e939a6a3b54c684b9d18235792c07e8
luizlcezario <luizlcezario@gmail.com>
2022-01-28 18:48:21 -0300

    tailwind_test

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,10 +1,6 @@
 import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
-
 * {
 	margin: 0;
 	padding: 0;
